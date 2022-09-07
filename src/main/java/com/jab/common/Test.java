2531ab92de901189d045c9f71feedc9e99a94f46
goshawker <goshawker@yeah.net>
2022-09-07 14:06:28 +0800

    创建架构

---
+++ src/main/java/com/jab/common/Test.java
@@ -0,0 +1,15 @@
+package com.jab.common;
+
+/**
+ * @Project: jab
+ * @Package: PACKAGE_NAME
+ * @Author: mike
+ * @CreateTime: 2022-09-07  14:00
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Test {
+  public static void main(String[] args){
+    System.out.printf("=============");
+  }
+}
